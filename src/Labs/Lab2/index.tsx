ca73d932ba9f7d5377b5832c8f94f252b72e8950
tracytai1999 <tracytai1999@gmail.com>
2024-10-02 16:40:20 -0700

    assignment2 labs complete

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,18 +1,101 @@
+import "./index.css";
+import "./ForegroundColors"
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from "./Margins";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import BootstrapGrids from './BootstrapGrids';
+import ReactIconsSampler from "./ReactIcons";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
+
+
 export default function Lab2() {
-    return (
-      <div id = "wd-lab2">
-        <h2>Lab 2</h2>
-        <h3>HTML Examples</h3>
-            <div id="wd-h-tag">
-                <h4>Heading Tags</h4>
-                Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short 
-                title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded 
-                by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. 
-                This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags 
-                can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different 
-                sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
-            </div>
+  return (
+    <div className="container">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE attribute</h3>
+      <p>
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
+      
+      <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="wd-id-selector-1">
+          Instead of changing the look and feel of all the 
+          elements of the same name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+          Here's another paragraph using a different ID and a different look and
+          feel
+        </p>
       </div>
-    );
+      
+      <div id="wd-css-class-selectors">
+        <h3>Class selectors</h3>
+        <p className="wd-class-selector">
+          Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+        </p>
+        <h4 className="wd-class-selector">
+          This heading has same style as paragraph above
+        </h4>
+      </div>
+      <div id="wd-css-document-structure">
+        <div className="wd-selector-1">
+          <h3>Document structure selectors</h3>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in particular
+            places in the document
+            <p className="wd-selector-3">
+              This paragraph's red background is referenced as
+              <br />
+              .selector-2 .selector3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                Whereas this span is a direct child of its parent
+              </span><br />
+                You can combine these relationships to create specific 
+                styles depending on the document structure
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div> <ForegroundColors/> </div><br/>
+      <div> <BackgroundColors/> </div><br/>
+      <div> <Borders/> </div><br/>
+      <div> <Padding/> </div><br/>
+      <div> <Margins/> </div><br/>
+      <div> <Corners/> </div><br/>
+      <div> <Dimensions/> </div><br/>
+      <div> <Positions/> </div><br/>
+      <div> <Zindex/> </div><br/>
+      <div> <Float/> </div><br/>
+      <div> <GridLayout/> </div><br/>
+      <div> <Flex/> </div><br/>
+      <div> <ReactIconsSampler/> </div><br/>
+      <div> <BootstrapGrids/> </div><br/>
+      <div> <ScreenSizeLabel/> </div><br/>
+      <div> <BootstrapTables/> </div><br/>
+      <div> <BootstrapLists/> </div><br/>
+      <div> <BootstrapForms/> </div><br/>
+      <div> <BootstrapNavigation/> </div><br/>
+
+
+    </div>
+  );
 }
-  
