95645badced03f1c21e9b0c968c27333715e2c6e
tracytai1999 <tracytai1999@gmail.com>
2024-10-15 13:14:33 -0700

    a3 adjustments

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -13,7 +13,7 @@ export default function Dashboard() {
               <div className="card rounded-3 overflow-hidden">
                 <Link to={`/Kanbas/Courses/${course._id}/Home`}
                       className="wd-dashboard-course-link text-decoration-none text-dark" >
-                  <img src={course.image} width="100%" height={160} />
+                  <img src={course.image} width="100%" height={160} alt="course_image"/>
                   <div className="card-body">
                     <h5 className="wd-dashboard-course-title card-title">
                       {course.name} </h5>
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -15,9 +15,9 @@ export default function KanbasNavigation() {
   return (
     <div id="wd-kanbas-navigation" style={{width: 120}}
          className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
+      <a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/" rel="noreferrer"
         className="list-group-item bg-black border-0 text-center">
-        <img src="/images/NEU.png" width="75px" /></a>
+        <img src="/images/NEU.png" width="75px" alt="Neu image"/></a>
       <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
             ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
         <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
