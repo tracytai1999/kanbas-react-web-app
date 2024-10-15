9b61371ae1f3fa91edbbe537e3480971d7a5b71a
tracytai1999 <tracytai1999@gmail.com>
2024-10-14 19:44:37 -0700

    finished labs

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,6 +1,6 @@
 export default function Lab1() {
     return (
-      <div id="wd-lab1">
+      <div id="wd-lab1" className="container">
             <h2>Lab 1</h2>
             <h3>HTML Examples</h3>
             <div id="wd-h-tag">
